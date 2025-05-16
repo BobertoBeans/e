d5e497f3267a6e5d50b489adaa982b8f3eba9a72
BobertoBeans <154119946+BobertoBeans@users.noreply.github.com>
2025-05-16 00:03:36 -0400

    Update bean.py

--- bean.py
+++ bean.py
@@ -2,7 +2,7 @@ from funktions import *
 
 print("          ╔═══════════════════════════╗")
 print("          ║      The Beancipherer     ║")
-print("          ║        By The Beanster         ║")
+print("          ║      By The Beanster      ║")
 print("          ╠═══════════════════════════╣")
 print("          ║ 1. Beancipher             ║")
 print("          ║ 2. Debeancipher           ║")
