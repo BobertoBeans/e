a95aa1e897914f18a7f9893d9b19fe841d4790d8
BobertoBeans <154119946+BobertoBeans@users.noreply.github.com>
2025-05-15 23:25:00 -0400

    Add files via upload

---
+++ func.py
@@ -0,0 +1,66 @@
+def bean():
+  print('Please enter the word you want to beancipher!');
+  word=input('');
+   
+  word=word.lower();
+   
+  wordlist=list(word);
+
+  wordlist=list(word);
+
+  alph='abcdefghijklmnopqrstuvwxyz';
+
+  n=0;
+  x=0;
+  y=0;
+
+  while(n<len(word)):
+      bool=False;
+      while(bool==False):
+         bool=word[x]==alph[y];
+         y+=1;
+      wordlist[x]=alph[y+x];
+      y=0;
+      x+=1;
+      n+=1;
+      bool=False;
+  wordlist=''.join(wordlist);
+  print(wordlist.upper());
+
+def debean():
+    print('Please enter the word you want to debeancipher!')
+    word=input('');
+   
+    word=word.lower();
+
+    wordlist=list(word);
+
+    alph='abcdefghijklmnopqrstuvwxyz';
+
+    n=0;
+    x=0;
+    y=0;
+
+    while(n<len(word)):
+      bool=False;
+      while(bool==False):
+         bool=word[x]==alph[y];
+         y-=1;
+      wordlist[x]=alph[y-x];
+      y=0;
+      x+=1;
+      n+=1;
+      bool=False;
+    wordlist=''.join(wordlist);
+    print(wordlist.upper());
+
+def menu():
+   print('1 or 2?'); x=input('');
+
+   if(x=='1'):
+         bean();
+   elif(x=='2'):
+         debean();
+   else:
+         print('Please enter 1 or 2!');
+         menu();
