02d796b73af8d2a3fc75b681d9b1c21b4b114f9c
BobertoBeans <154119946+BobertoBeans@users.noreply.github.com>
2025-07-28 00:59:20 -0400

    Update Brog 2.0.py

--- brog/Brog 2.0.py
+++ brog/Brog 2.0.py
@@ -1,1 +1,19 @@
-
+def brog():
+  x=input('What word do you want to brogify?\n');
+  lX=list(x);
+  y=0
+  n=0;
+  if(len(x)<3):
+    print('Not a big enough word!')
+    brog()
+  for n in x:
+    lX[y]='o';
+    y+=1;
+  lX[0]='B';
+  lX[1]='r';
+  lX[-1]='g';
+  x2=''.join(lX);
+  print(x2)
+  print(x2[::-1])
+  brog()
+brog()
